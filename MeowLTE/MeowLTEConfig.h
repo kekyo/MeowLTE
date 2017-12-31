db4ae6d20b3f8801ed4d96837a15a56c2ad5dc92
Kouji Matsui <k@kekyo.net>
2017-12-31 09:31:39 +0900

    Fixed lack of declarations.

--- MeowLTE/MeowLTEConfig.h
+++ MeowLTE/MeowLTEConfig.h
@@ -1,2 +1,12 @@
+
+#define APN      "so-net.jp"
+#define USERNAME "nuro"
+#define PASSWORD "nuro"
+
+//#define APN      "soracom.io"
+//#define USERNAME "sora"
+//#define PASSWORD "sora"
+
 #define IFTTT_MAKER_WEBHOOKS_EVENT   "<Replace your event name>"
 #define IFTTT_MAKER_WEBHOOKS_KEY     "<Replace your key>"
+
